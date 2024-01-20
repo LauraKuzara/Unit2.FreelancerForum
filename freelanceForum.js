88c1156419820d7c697b050e7f8d04fd44b866c2
LauraKuzara <kuzaralaura@gmail.com>
2024-01-20 14:20:47 -0500

    made the average

--- freelanceForum.js
+++ freelanceForum.js
@@ -12,10 +12,18 @@ make a max amount of freelancers to populate
 find the average of the freelancers starting rate
 */
 
-// const freelancers = [
-//     { name: "Dr. Slice", price: 25, occupation: "gardener" },
-//     { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-// ];
+const freelancers = [
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+];
+
+function calculateAveragePrice() {
+    const sum = freelancers.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue.price;
+    }, 0);
+    const average = sum / freelancers.length;
+    return average;
+}
 
 function render() {
     // Render the freelancers
@@ -28,6 +36,30 @@ function render() {
         <th>Occupation</th>
         <th>Starting Price</th>
         </tr>`;
+
+        freelancers.forEach((freelancer) => {
+            const tr = document.createElement("tr");
+            const tdName = document.createElement("td");
+            tdName.textContent = freelancer.name;
+        
+            const tdOccupation = document.createElement("td");
+            tdOccupation.textContent = freelancer.occupation;
+        
+            const tdPrice = document.createElement("td");
+            tdPrice.classList.add("price");
+            tdPrice.textContent = `$${freelancer.price}`;
+        
+            tr.appendChild(tdName);
+            tr.appendChild(tdOccupation);
+            tr.appendChild(tdPrice);
+        
+            freelancersId.appendChild(tr);
+        
+            const average = document.querySelector("#average");
+            average.innerHTML = `The average starting price is: $${calculateAveragePrice().toFixed(
+            2
+            )}`;
+        });
 };
 
-render()
+render();
