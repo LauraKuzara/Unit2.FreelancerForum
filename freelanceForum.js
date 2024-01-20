205d72897006429f43000960ccdf0f17e4dea53f
LauraKuzara <kuzaralaura@gmail.com>
2024-01-20 13:32:31 -0500

    created freelancer arrayand started render function

---
+++ freelanceForum.js
@@ -0,0 +1,27 @@
+/*
+create a pre-populated list (array) of freelancers
+randomly select an occupation, cost, into freelancer array
+must use `document.querySelector` correctly to select existing 
+DOM elements and update the average starting price
+
+add items into the freelancers array at a certain cadence
+create render function
+
+make a max amount of freelancers to populate
+
+find the average of the freelancers starting rate
+*/
+
+const freelancers = [
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  ];
+
+const macFreelancers = 10;
+
+  function render() {
+    // Render the squares
+    const querySelectedFreeLancers = document.querySelector("#freelancers");
+     
+  }
+  //and the render?
